1056e1b0b6ecbd1ba6fcb1d42527c1e0247a2339
javi <javii.oteero@gmail.com>
2024-05-07 13:30:02 +0200

    primeros 4 tests pasados y refactor

--- src/PasswordVerify.java
+++ src/PasswordVerify.java
@@ -2,10 +2,12 @@ public class PasswordVerify {
 
     public static EsCorrecta verify(String password){
         StringBuilder sb = new StringBuilder();
-        boolean length;
-        length = password.length() >= 8;
+        boolean length = length = password.length() >= 8;;
+        boolean nums = password.matches(".*[0-9].*[0-9].*");
+
         if (!length)
             sb.append("La contrasenya ha de tenir almenys 8 caràcters\n");
+        if (!nums)
             sb.append("La contrasenya ha de contenir almenys 2 números\n");
             sb.append("La contrasenya ha de contenir almenys una lletra majúscula\n");
             sb.append("La contrasenya ha de contenir almenys un caràcter especial");
