e5551a76a6d7971428371900c7befc8db3561a7f
javi <javii.oteero@gmail.com>
2024-05-07 13:32:29 +0200

    primeros 6 tests pasados

--- src/PasswordVerify.java
+++ src/PasswordVerify.java
@@ -5,12 +5,14 @@ public class PasswordVerify {
         boolean length = length = password.length() >= 8;;
         boolean nums = password.matches(".*[0-9].*[0-9].*");
         boolean mayus = password.matches(".*[A-Z].*");
+        boolean especial = password.matches(".*[!@#%&/()=+?~–^].*");
         if (!length)
             sb.append("La contrasenya ha de tenir almenys 8 caràcters\n");
         if (!nums)
             sb.append("La contrasenya ha de contenir almenys 2 números\n");
         if (!mayus)
             sb.append("La contrasenya ha de contenir almenys una lletra majúscula\n");
+        if (!especial)
             sb.append("La contrasenya ha de contenir almenys un caràcter especial");
         return new EsCorrecta(false, sb.toString());
     }
