1ea4c1912341f672ec609a6500e1f3629ac2f4c7
javi <javii.oteero@gmail.com>
2024-05-07 13:31:14 +0200

    primeros 5 tests pasados

--- src/PasswordVerify.java
+++ src/PasswordVerify.java
@@ -4,11 +4,12 @@ public class PasswordVerify {
         StringBuilder sb = new StringBuilder();
         boolean length = length = password.length() >= 8;;
         boolean nums = password.matches(".*[0-9].*[0-9].*");
-
+        boolean mayus = password.matches(".*[A-Z].*");
         if (!length)
             sb.append("La contrasenya ha de tenir almenys 8 caràcters\n");
         if (!nums)
             sb.append("La contrasenya ha de contenir almenys 2 números\n");
+        if (!mayus)
             sb.append("La contrasenya ha de contenir almenys una lletra majúscula\n");
             sb.append("La contrasenya ha de contenir almenys un caràcter especial");
         return new EsCorrecta(false, sb.toString());
